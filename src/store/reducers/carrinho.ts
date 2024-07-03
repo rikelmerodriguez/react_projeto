4d27dead912fbe8e14bf3a5a886a26872b3344de
jonesnambunndo@hotmail.com <jonesnambundo@mtel.inatel.br>
2024-07-03 03:26:53 -0300

    Mesclando alterações do repositório original

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,30 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+      const existingProduct = state.itens.find((p) => p.id === produto.id)
+
+      if (existingProduct) {
+        // Handle alert outside of reducer for better state management
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
